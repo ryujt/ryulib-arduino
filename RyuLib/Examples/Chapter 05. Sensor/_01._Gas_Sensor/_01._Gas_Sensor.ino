4a25f423ac47719f14b61c6bab4537a4a0d02cdd
Ryu <ryujt658@gmail.com>
2016-10-13 02:31:15 +0900

    가스 센서 소스 추가

---
+++ RyuLib/Examples/Chapter 05. Sensor/_01._Gas_Sensor/_01._Gas_Sensor.ino
@@ -0,0 +1,35 @@
+#include <TM1637Display.h>
+
+TM1637Display display(2, 3);
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(4, INPUT); 
+  pinMode(13, INPUT); 
+
+  display.setBrightness(0);
+}
+
+int max = 0;
+
+void loop() {
+  int value = analogRead(0);
+  bool is_detected = digitalRead(4) == 0;
+  
+  Serial.println(value); 
+
+  if (is_detected) {
+    if (value > max) max = value;
+    display.setBrightness(15);
+    display.showNumberDec( max / 2 );
+    tone(13, 400);
+  } else {
+    max = 0;
+    display.setBrightness(0);
+    display.showNumberDec( 0 );
+    noTone(13);
+  }
+  
+  delay(10);
+}
