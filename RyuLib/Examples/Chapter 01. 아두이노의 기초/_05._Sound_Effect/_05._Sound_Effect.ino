a76b8f2bde5d42dcc4eb8eee70fc01636e1f8f1d
Ryu <ryujt658@gmail.com>
2016-10-18 00:27:29 +0900

    폴더 구조 변경 및 중복 제거

--- RyuLib/Examples/Chapter 01. 아두이노의 기초/_05._Sound_Effect/_05._Sound_Effect.ino
+++ RyuLib/Examples/Chapter 01. 아두이노의 기초/_05._Sound_Effect/_05._Sound_Effect.ino
@@ -8,12 +8,12 @@ void loop() {
 	seAriel(13);
 	delay(1000);
 
-	sePhone(13);
-	delay(1000);
-
-	seSiren(13);
-	delay(1000);
-
-	seAlarm(13);
-	delay(1000);
+//	sePhone(13);
+//	delay(1000);
+//
+//	seSiren(13);
+//	delay(1000);
+//
+//	seAlarm(13);
+//	delay(1000);
 }
