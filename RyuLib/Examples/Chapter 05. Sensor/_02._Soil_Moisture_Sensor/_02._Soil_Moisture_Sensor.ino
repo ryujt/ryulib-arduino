9e77614b49fc718c4e71b84f3a584113bfee4e92
Ryu <ryujt658@gmail.com>
2016-10-28 23:14:21 +0900

    토양 센서 예제 추가

---
+++ RyuLib/Examples/Chapter 05. Sensor/_02._Soil_Moisture_Sensor/_02._Soil_Moisture_Sensor.ino
@@ -0,0 +1,17 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int humidity = analogRead(0);
+
+  Serial.println(humidity);
+
+  if (humidity > 600) {
+    tone(13, 1000);
+    delay(1000);
+    noTone(13);
+  }
+
+  delay(200);
+}
