873e178c1a5ab10335618bafef15c567dd15368b
Ryu <ryujt658@gmail.com>
2016-11-23 18:35:16 +0900

    * LED array 제거 * MP3 쉴드 추가

---
+++ RyuLib/Projects/Cat_Laser/Cat_Laser.ino
@@ -0,0 +1,31 @@
+#include <Servo.h> 
+
+Servo servo_01; 
+Servo servo_02; 
+
+void setup() {
+  pinMode(0, OUTPUT);
+  digitalWrite(0, HIGH);
+  
+  servo_01.attach( 9);
+  servo_02.attach(10);
+
+  servo_01.write(0); 
+  servo_02.write(0); 
+}
+
+double angle;
+
+void loop() {
+  for(angle = 0; angle < 180; angle++) { 
+    servo_01.write(angle); 
+    servo_02.write( sin(angle / 10) * 20 + 30 ); 
+    delay(20); 
+  } 
+
+  for(angle = 180; angle > 0; angle--) { 
+    servo_01.write(angle); 
+//    servo_02.write(angle); 
+    delay(20); 
+  } 
+}
