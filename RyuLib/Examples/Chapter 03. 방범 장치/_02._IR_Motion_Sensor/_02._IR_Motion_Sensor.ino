0ebae751b413010dd6c45ded43e16eb07b11220d
Ryu <ryujt658@hanmail.net>
2016-12-30 17:55:21 +0900

    적외선 센서를 예제 수정, 좀 더 민첩하게 동작하도록

--- RyuLib/Examples/Chapter 03. 방범 장치/_02._IR_Motion_Sensor/_02._IR_Motion_Sensor.ino
+++ RyuLib/Examples/Chapter 03. 방범 장치/_02._IR_Motion_Sensor/_02._IR_Motion_Sensor.ino
@@ -4,12 +4,11 @@ void setup() {
 }
 
 void loop() {
-  // TODO: 속도를 빠르게 하고, 라이브러리를 제공하여 쉽게 
-  if (digitalRead(12) == HIGH) {       
-    tone(13, 1000);
-    delay(1000);
-    noTone(13);
+  if (digitalRead(12) == HIGH) {
+      tone(13, 1000);
+  } else {
+    noTone(13);    
   }
-  
-  delay(200);
+
+  delay(50);
 }
