e985360c641645465fd062f2d3f2c104f077283c
Ryu <ryujt658@hanmail.net>
2017-01-19 19:18:10 +0900

    Cat laser 예제의 매뉴얼 추가

--- RyuLib/Examples/Chapter 06. RC and Robot/_07._Cat_Laser/_07._Cat_Laser.ino
+++ RyuLib/Examples/Chapter 06. RC and Robot/_07._Cat_Laser/_07._Cat_Laser.ino
@@ -7,8 +7,8 @@ void setup() {
   pinMode(0, OUTPUT);
   digitalWrite(0, HIGH);
   
-  servo_01.attach( 9);
-  servo_02.attach(10);
+  servo_01.attach(9);
+  servo_02.attach(8);
 
   servo_01.write(0); 
   servo_02.write(0); 
@@ -20,12 +20,11 @@ void loop() {
   for(angle = 0; angle < 180; angle++) { 
     servo_01.write(angle); 
     servo_02.write( sin(angle / 10) * 20 + 30 ); 
-    delay(20); 
+    delay(40); 
   } 
 
   for(angle = 180; angle > 0; angle--) { 
     servo_01.write(angle); 
-//    servo_02.write(angle); 
-    delay(20); 
+    delay(40); 
   } 
 }
