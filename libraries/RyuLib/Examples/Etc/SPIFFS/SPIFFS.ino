f1ade989b76af456167d414260f459cd9a7d6bfa
Ryu <ryujt658@hanmail.net>
2017-03-27 22:43:56 +0900

    NodeMCU SPIFFS 예제

---
+++ libraries/RyuLib/Examples/Etc/SPIFFS/SPIFFS.ino
@@ -0,0 +1,27 @@
+#include "FS.h"
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("\nVery basic Spiffs example, writing 10 lines to SPIFFS filesystem, and then read them back");
+
+  SPIFFS.begin();
+  
+//  Serial.println("Please wait for SPIFFS to be formatted");
+//  SPIFFS.format();
+//  Serial.println("Spiffs formatted");
+//
+//  File f = SPIFFS.open("/f.txt", "w");
+//  if (!f) Serial.println("file open failed");
+//  f.println("Hello?");
+//  f.close();
+}
+
+void loop() {
+  File f = SPIFFS.open("/f.txt", "r");
+  if (!f) Serial.println("file open failed");
+  String s = f.readStringUntil('\n');
+  Serial.println(s);
+  f.close();
+
+  delay(3000);
+} 
