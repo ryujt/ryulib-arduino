b6e71732e2d1cb68ecd708c5ea104f5b99ddee7c
Ryu <ryujt658@hanmail.net>
2017-05-07 11:23:14 +0900

    I2C scanner

---
+++ libraries/RyuLib/Examples/Etc/i2c_scanner/i2c_scanner.ino
@@ -0,0 +1,34 @@
+#include <Wire.h>
+void setup() {
+   Serial.begin (9600);
+   while (!Serial) { }
+
+   Serial.println ();
+   Serial.println ("I2C scanner. Scanning ...");
+
+   byte count = 0;
+   Wire.begin();
+   for (byte i = 1; i < 120; i++){
+     Wire.beginTransmission(i);
+     if (Wire.endTransmission() == 0){
+       Serial.print ("Found address: ");
+       Serial.print (i, DEC);
+       Serial.print (" (0x");
+       Serial.print (i, HEX);
+       Serial.println (")");
+       count++;
+       delay (1);
+     }
+ }
+ 
+ Serial.println ("Done.");
+ Serial.print ("Found ");
+ Serial.print (count, DEC);
+ Serial.println (" device(s).");
+}
+
+void loop()
+{
+  
+}
+
