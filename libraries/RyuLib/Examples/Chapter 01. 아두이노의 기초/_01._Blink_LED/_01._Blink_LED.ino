b8ebc8d2560412f89beeebed3f097f64de87dbdd
Ryu <ryujt658@hanmail.net>
2017-08-09 20:01:35 +0900

    1권 배치도(회로도) 수정

--- libraries/RyuLib/Examples/Chapter 01. 아두이노의 기초/_01._Blink_LED/_01._Blink_LED.ino
+++ libraries/RyuLib/Examples/Chapter 01. 아두이노의 기초/_01._Blink_LED/_01._Blink_LED.ino
@@ -1,11 +1,11 @@
 void setup() {
-  pinMode(13, OUTPUT);
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(13, HIGH);
+  digitalWrite(2, HIGH);
   delay(1000);
   
-  digitalWrite(13, LOW);
+  digitalWrite(2, LOW);
   delay(1000);
 }
